0707073901bb35dd1f75b08f50230118f5583e94
Herb Wildfeuer <wildfirenet@gmail.com>
2018-04-22 21:13:53 -0700

    add font weight to text style

--- lib/apple-news/styles/text.rb
+++ lib/apple-news/styles/text.rb
@@ -2,7 +2,8 @@ module AppleNews
   module Style
     class Text < Base
       optional_properties :background_color, :font_name, :font_size, :text_color, :text_shadow,
-                          :strikethrough, :stroke, :tracking, :underline, :vertical_alignment
+                          :strikethrough, :stroke, :tracking, :underline, :vertical_alignment,
+                          :font_weight
     end
   end
 end
